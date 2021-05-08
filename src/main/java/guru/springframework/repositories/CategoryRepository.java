58e4ec449bc34747015185eb839448521e7d549e
rogers-ch <crogers25@mail.greenriver.edu>
2021-05-07 17:10:15 -0700

    Video 141 work completed - Spring Data JPA Query Methods

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by cr on 05/07/2021
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by cr on 05/07/2021
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
